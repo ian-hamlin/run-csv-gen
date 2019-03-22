ea7d0b1204836b1cfb0dd0665c67eea86011c1b0
ian-hamlin <ian-hamlin@users.noreply.github.com>
2019-03-22 20:21:54 +0000

    remove and re-create file on run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{error::Error, fs::File};
+use std::{
+    error::Error,
+    fs::{remove_file, OpenOptions},
+    path::Path,
+};
+
+pub const EXPORT_FILE_NAME: &str = "racing_paces.csv";
 
 fn main() {
     match create_csv() {
@@ -10,8 +16,17 @@ fn main() {
 }
 
 fn create_csv() -> Result<(), Box<Error>> {
+    // try and delete existing files.
+    if Path::new(EXPORT_FILE_NAME).exists() {
+        remove_file(EXPORT_FILE_NAME)?;
+    }
+
     // Open the file.
-    let _file = File::open("running_paces.csv")?;
+    let _writer = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(false)
+        .open(EXPORT_FILE_NAME)?;
 
     Ok(())
 }
