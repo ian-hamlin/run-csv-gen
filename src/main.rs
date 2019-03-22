d44c60e7ed6db6c119add9194da901295284a67f
ian-hamlin <ian-hamlin@users.noreply.github.com>
2019-03-22 20:13:55 +0000

    structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{error::Error, fs::File};
+
 fn main() {
-    println!("Hello, world!");
+    match create_csv() {
+        Ok(()) => {}
+        Err(err) => {
+            eprintln!("Failed to process file {}", err);
+        }
+    };
+}
+
+fn create_csv() -> Result<(), Box<Error>> {
+    // Open the file.
+    let _file = File::open("running_paces.csv")?;
+
+    Ok(())
 }
